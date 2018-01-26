31ae5dbdb7261fcb350d9c931ae1b253238903ec
Jonny Barnes <jonny@jonnybarnes.uk>
2018-01-26 22:46:17 +0000

    Dummy entry for new media, add toggle-able reply element

--- resources/assets/js/components/NewNoteFormComponent.vue
+++ resources/assets/js/components/NewNoteFormComponent.vue
@@ -1,11 +1,16 @@
 <template>
     <form :action="action" method="post">
         <input type="hidden" name="_token" :value="csrf">
+        <div v-if="showReply">
+            <label for="reply">Reply To:</label>
+            <input name="reply" id="reply">
+        </div>
+        <button type="button" v-if="showReply == false" v-on:click="showReply = !showReply">Reply</button>
         <div>
             <label for="note">Note:</label>
             <textarea name="note" id="note"></textarea>
         </div>
-        <div>
+        <div v-if="targets">
             <fieldset>
                 <legend>Syndication</legend>
                 <template v-for="target in targets">
@@ -13,6 +18,9 @@
                     <input :id="target.uid" name="mp-syndicate-to[]" type="checkbox" :value="target.uid">
                 </template>
             </fieldset>
+        </div>
+        <div v-if="media">
+            <p>Media Upload to <code>{{ media }}</code></p>
         </div>
         <button type="submit" name="submit">Submit</button>
     </form>
@@ -28,12 +36,21 @@
                 type: String,
                 required: true,
             },
+            media: {
+                type: String,
+                required: false,
+            },
             targets: {
                 validator: function (value) {
                     return value[0].hasOwnProperty('uid')
                         && value[0].hasOwnProperty('name');
                 },
                 required: false,
+            }
+        },
+        data: function () {
+            return {
+                showReply: false,
             }
         }
     }
